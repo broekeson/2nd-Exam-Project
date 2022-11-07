5701e961cffe19b0f255a7bfdabad9145d18c143
Ekene Ikogbwe <broekeson@gmail.com>
2022-11-07 19:09:58 +0100

    updates

--- files/postgree_db.sh
+++ files/postgree_db.sh
@@ -1,31 +1,27 @@
 #!/usr/bin/bash
-
-# Install ca-certificates
-apt-get install ca-certificates
-
 # Add apt key
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 
 # Add official Postgres repository
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 # Update apt
 apt-get update
 
 # Install Postgres and Postgres contrib
-apt-get install postgresql postgresql-contrib 
+apt-get install postgresql postgresql-contrib -y
 
-# Create a new user
-sudo -u postgres createuser --interactive
+# Switch to root directory
+cd /
+
+# Create a new user from the command line by a root user
+sudo -u postgres createuser ekene
 
 # Create a new database
-sudo -u postgres createdb mydb
+sudo -u postgres createdb ekenedb
 
-# Connect to the database
-sudo -u postgres psql mydb
-
-# exit the database
-\q
+# connect the database with the user and grant all privileges
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ekenedb TO ekene"
 
 # Restart Postgres
 sudo service postgresql restart
